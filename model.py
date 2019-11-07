bff48c155d28d7b6512e77e43df37db806450584
KSXGroup <kstarxin@gmail.com>
2019-11-07 23:15:42 +0800

    update

--- model.py
+++ model.py
@@ -4,7 +4,7 @@ from torch import nn
 
 class QAModel(nn.Module):
     def __init__(self, encoder_config, decoder_config):
-        super(QAModel, self).__init__()
+        super().__init__()
         self.encoder = ALBERT(**encoder_config)
         self.decoder = nn.Linear(encoder_config["hidden_size"], decoder_config["num_label"])
 
--- run_record.py
+++ run_record.py
@@ -6,7 +6,7 @@ from torch.nn import CrossEntropyLoss
 from data.create_record_data import InputFeatures
 from data.record_utils import load_record_dataset, QADataset
 from ALBERT.train.optimizer import AdamW, WarmupLinearSchedule
-from torch.utils.data.dataloader import DataLoader
+from torch.utils.data import DataLoader, RandomSampler
 from model import QAModel
 
 
@@ -19,16 +19,20 @@ def main(args):
     model = QAModel(encoder_config, decoder_config)
     device = torch.device('cuda:3')
     model.to(device)
+    #print(list(model.named_parameters()))
+    #return
     dataset = load_record_dataset(args.train_file, encoder_config["max_sequence_length"])
-    dataloader = DataLoader(dataset=dataset, batch_size=train_config["batch_size"], num_workers=4)
+    dataloader = DataLoader(dataset=dataset, sampler= RandomSampler(dataset),
+                            batch_size=train_config["batch_size"], num_workers=4)
+
     param_optimizer = list(model.named_parameters())
-    print(len(param_optimizer))
-    no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
+    no_decay = ['bias', 'layer_norm.bias', 'layer_norm.weight']
     optimizer_grouped_parameters = [
         {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)],
          'weight_decay': train_config["weight_decay"]},
         {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
     ]
+
 
     total_steps = int(len(dataset) / train_config["batch_size"]) * train_config["epoch"]
 
@@ -37,15 +41,18 @@ def main(args):
     optimizer = AdamW(optimizer_grouped_parameters, lr=train_config["lr"], eps=train_config["lr_decay"])
     lr_scheduler = WarmupLinearSchedule(optimizer, warmup_steps= int(total_steps * train_config["warm_up_proportion"]),
                                         t_total=total_steps)
+    #optimizer = torch.optim.Adam(model.encoder.parameters())
     optimizer.zero_grad()
+    model.train()
+    start_loss_function = CrossEntropyLoss()
+    end_loss_function = CrossEntropyLoss()
 
     for epoch in range(train_config["epoch"]):
         for i, batch in enumerate(dataloader):
             (tokens, masks, seg_ids, start_postions, end_positions) = tuple(x.to(device) for x in batch)
-            pos_ids = torch.tensor(np.arange(encoder_config["max_sequence_length"]), device=device)
+            pos_ids = torch.tensor(np.arange(tokens.shape[1]), dtype=torch.int64, device=tokens.device)
             start_pos_output, end_pos_output = model(tokens, seg_ids, pos_ids, masks)
-            start_loss_function = CrossEntropyLoss()
-            end_loss_function = CrossEntropyLoss()
+            #print(start_pos_output, end_pos_output)
             start_loss = start_loss_function(start_pos_output, start_postions)
             end_loss = end_loss_function(end_pos_output, end_positions)
             loss = (torch.mean(start_loss) + torch.mean(end_loss)) / 2.0
@@ -53,7 +60,7 @@ def main(args):
             optimizer.step()
             lr_scheduler.step()
             optimizer.zero_grad()
-            print(loss.item())
+            print(str(i) +":"+ str(loss.item()))
 
 
 if __name__ == "__main__":
